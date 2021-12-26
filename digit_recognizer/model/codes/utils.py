99e63f2c33245a2b8f3cf43c5a9c7b19c56dda24
Preetham Ganesh <preetham.ganesh2015@gmail.com>
2021-12-26 16:07:59 -0600

    added model_training_and_evaluation

--- digit_recognizer/model/codes/utils.py
+++ digit_recognizer/model/codes/utils.py
@@ -29,25 +29,27 @@ def data_splitting(original_data: pd.DataFrame):
     return new_train_data, new_validation_data, new_test_data
 
 
-def data_preprocessing(new_input_data: pd.DataFrame):
+def data_preprocessing(new_data: pd.DataFrame):
     """Converts new split dataframe into input and target data. Performs data transformation on the new input data.
 
         Args:
-            new_input_data: New split dataframe which contains the label and pixel values for the image.
+            new_data: New split dataframe which contains the label and pixel values for the image.
 
         Returns:
             A tuple which contains 2 Tensors for the new_input_data and new_target_data.
     """
-    if 'label' in new_input_data.columns():
+    if 'label' in list(new_data.columns):
         # Splits the data into the input and target data
-        new_input_data = new_input_data.drop(columns=['label'])
-        new_target_data = list(new_input_data['label'])
+        new_input_data = new_data.drop(columns=['label'])
+        new_target_data = list(new_data['label'])
+    else:
+        new_input_data = new_data
 
     # Reshapes the input data, converts into float32 format, normalizes the pixel values and converts to tensor.
     new_input_data = np.array(new_input_data).reshape((len(new_input_data), 28, 28, 1)).astype('float32') / 255
     new_input_data = tf.convert_to_tensor(new_input_data)
 
-    if 'label' in new_input_data.columns():
+    if 'label' in list(new_data.columns):
         new_target_data = tf.keras.utils.to_categorical(new_target_data)
         return new_input_data, new_target_data
 
@@ -116,5 +118,54 @@ def choose_model(model_number: int):
         print('The model number entered is incorrect. Kindly enter the right model number')
         sys.exit()
     return model
+
+
+def model_training_and_evaluation(new_train_input_data: tf.Tensor,
+                                  new_train_target_data: tf.Tensor,
+                                  new_validation_input_data: tf.Tensor,
+                                  new_validation_target_data: tf.Tensor,
+                                  new_test_input_data: tf.Tensor,
+                                  new_test_target_data: tf.Tensor,
+                                  configuration: dict):
+    """Uses configuration to choose the model, train and evaluate the model.
+
+        Args:
+            new_train_input_data: Transformed input training data of shape(x, 28, 28, 1)
+            new_train_target_data: Transformed target training data of shape (x, 10)
+            new_validation_input_data: Transformed input validation data of shape(x, 28, 28, 1)
+            new_validation_target_data: Transformed target validation data of shape (x, 10)
+            new_test_input_data: Transformed input testing data of shape(x, 28, 28, 1)
+            new_test_target_data: Transformed target testing data of shape (x, 10)
+            configuration: Dictionary which contains details for training and testing of the model.
+
+        Returns:
+            None
+    """
+    # Chooses the model based configuration file.
+    model = choose_model(configuration['model'])
+    print(model.summary())
+    print()
+
+    # Creates model checkpoint callback to monitor and save best model.
+    directory_path = '../results'
+    checkpoint_directory = '{}/{}_{}/{}/{}'.format(directory_path, 'model', configuration['model'],
+                                                   'checkpoint_directory', 'checkpoint')
+    model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_directory,
+                                                                   save_weights_only=True, monitor='val_loss',
+                                                                   mode='min', verbose=1)
+
+    # Trains model using the training data, validates using the validation data, & uses callback to save the best model.
+    history = model.fit(x=new_train_input_data, y=new_train_target_data, batch_size=configuration['batch_size'],
+                        epochs=configuration['epochs'], verbose=2, callbacks=model_checkpoint_callback,
+                        validation_data=(new_validation_input_data, new_validation_target_data))
+    print()
+    print(history.history)
+    print()
+
+    # Evaluates the model using the testing data.
+    test_score = model.evaluate(new_test_input_data, new_test_target_data)
+    print('Test loss: {}'.format(round(test_score[0], 3)))
+    print('Test accuracy: {}'.format(round(test_score[1], 3)))
+    print()
 
 
