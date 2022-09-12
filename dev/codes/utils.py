63e795d89272be4e01b9946be1ba0642face8920
Preetham Ganesh <preetham.ganesh2021@gmail.com>
2022-09-11 21:36:13 -0500

    added check_directory_path_existence function

--- dev/codes/utils.py
+++ dev/codes/utils.py
@@ -23,3 +23,23 @@ import time
 from model import DigitRecognition
 
 
+physical_devices = tf.config.list_physical_devices('GPU')
+tf.config.experimental.set_memory_growth(physical_devices[0], enable=True)
+
+
+def check_directory_path_existence(directory_path: str) -> str:
+    """Creates the absolute path for the directory path given in argument if it does not already exist.
+
+    Args:
+        directory_path: A string which contains the directory path that needs to be created if it does not already 
+            exist.
+
+    Returns:
+        A string which contains the absolute directory path.
+    """
+    # Creates the following directory path if it does not exist.
+    home_directory_path = os.path.dirname(os.getcwd())
+    absolute_directory_path = '{}/{}'.format(home_directory_path, directory_path)
+    if not os.path.isdir(absolute_directory_path):
+        os.makedirs(absolute_directory_path)
+    return absolute_directory_path
