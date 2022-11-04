1e53e623ab19c49dd0e7f3def1067ad3f7b126d6
Preetham Ganesh <preetham.ganesh2015@gmail.com>
2022-11-04 16:24:07 -0500

    added create_log function

--- utils/utils.py
+++ utils/utils.py
@@ -2,10 +2,9 @@
 
 
 import os
+import logging
 
 import json
-
-from utils.logger import log_information
 
 
 def check_directory_path_existence(directory_path: str) -> str:
@@ -26,6 +25,34 @@ def check_directory_path_existence(directory_path: str) -> str:
     if not os.path.isdir(absolute_directory_path):
         os.makedirs(absolute_directory_path)
     return absolute_directory_path
+
+
+def create_log(logger_directory_path: str, log_file_name: str) -> None:
+    """Creates an object for logging terminal output.
+
+    Args:
+        logger_directory_path: A string which contains the location where the log file should be stored.
+        log_file_name: A string which contains the name for the log file.
+
+    Returns:
+        None.
+    """
+    # Type checks arguments.
+    assert isinstance(logger_directory_path, str), "Argument is not of type string."
+    assert isinstance(log_file_name, str), "Argument is not of type string."
+
+    # Checks if the following path exists.
+    logger_directory_path = check_directory_path_existence(logger_directory_path)
+
+    # Create and configure logger
+    logging.basicConfig(
+        filename="{}/{}.log".format(logger_directory_path, log_file_name),
+        format="%(asctime)s %(message)s",
+        filemode="w",
+    )
+    global logger
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
 
 
 def save_json_file(dictionary: dict, file_name: str, directory_path: str) -> None:
