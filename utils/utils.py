a42120dee5cc7af3928ee88c970fb9623b3784ba
Preetham Ganesh <preetham.ganesh2015@gmail.com>
2022-11-04 13:33:56 -0500

    added heck_directory_path_existence function

---
+++ utils/utils.py
@@ -0,0 +1,23 @@
+# authors_name = 'Preetham Ganesh'
+# project_title = 'Digit Recognizer'
+# email = 'preetham.ganesh2021@gmail.com'
+
+
+import os
+
+
+def check_directory_path_existence(directory_path: str) -> str:
+    """Creates the absolute path for the directory path given in argument if it does not already exist.
+
+    Args:
+        directory_path: A string which contains the directory path that needs to be created if it does not already exist.
+
+    Returns:
+        A string which contains the absolute directory path.
+    """
+    # Creates the following directory path if it does not exist.
+    home_directory_path = os.getcwd()
+    absolute_directory_path = "{}/{}".format(home_directory_path, directory_path)
+    if not os.path.isdir(absolute_directory_path):
+        os.makedirs(absolute_directory_path)
+    return absolute_directory_path
