2db74f9aec8505be6ab5d52cb22da6d83aafe091
Preetham Ganesh <preetham.ganesh2015@gmail.com>
2022-11-04 15:47:24 -0500

    added save_json_file function

--- utils/utils.py
+++ utils/utils.py
@@ -1,9 +1,11 @@
-# authors_name = 'Preetham Ganesh'
-# project_title = 'Digit Recognizer'
-# email = 'preetham.ganesh2021@gmail.com'
+# Copyright © 2022 Preetham Ganesh.
 
 
 import os
+
+import json
+
+from utils.logger import log_information
 
 
 def check_directory_path_existence(directory_path: str) -> str:
@@ -15,9 +17,39 @@ def check_directory_path_existence(directory_path: str) -> str:
     Returns:
         A string which contains the absolute directory path.
     """
+    # Type checks arguments.
+    assert isinstance(directory_path, str), "Argument is not of type string."
+
     # Creates the following directory path if it does not exist.
     home_directory_path = os.getcwd()
     absolute_directory_path = "{}/{}".format(home_directory_path, directory_path)
     if not os.path.isdir(absolute_directory_path):
         os.makedirs(absolute_directory_path)
     return absolute_directory_path
+
+
+def save_json_file(dictionary: dict, file_name: str, directory_path: str) -> None:
+    """Converts a dictionary into a JSON file and saves it for future use.
+
+    Args:
+        dictionary: A dictionary which needs to be saved.
+        file_name: A string which contains the name with which the file has to be saved.
+        directory_path: A string which contains the path where the file needs to be saved.
+
+    Returns:
+        None.
+    """
+    # Types checks arguments.
+    assert isinstance(dictionary, dict), "Argument is not of type dictionary."
+    assert isinstance(file_name, str), "Argument is not of type string."
+    assert isinstance(directory_path, str), "Argument is not of type string."
+
+    # Creates the following directory path if it does not exist.
+    directory_path = check_directory_path_existence(directory_path)
+
+    # Saves the dictionary or list as a JSON file at the file path location.
+    file_path = "{}/{}.json".format(directory_path, file_name)
+    with open(file_path, "w") as out_file:
+        json.dump(dictionary, out_file, indent=4)
+    out_file.close()
+    log_information("{} file saved successfully at {}.".format(file_name, file_path))
