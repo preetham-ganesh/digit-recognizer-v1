ebe3fb329938bd732ceca73130b4c5d121e319d5
Preetham Ganesh <preetham.ganesh2015@gmail.com>
2022-11-04 16:24:36 -0500

    added log_information function

--- utils/utils.py
+++ utils/utils.py
@@ -55,6 +55,29 @@ def create_log(logger_directory_path: str, log_file_name: str) -> None:
     logger.setLevel(logging.DEBUG)
 
 
+def log_information(log: str) -> None:
+    """Saves current log information, and prints it in terminal.
+
+    Args:
+        log: A string which contains the information that needs to be printed in terminal and saved in log.
+
+    Returns:
+        None.
+
+    Exception:
+        NameError: When the logger is not defined, this exception is thrown.
+    """
+    # Type checks arguments.
+    assert isinstance(log, str), "Argument is not of type string."
+
+    # Adds current log into log file.
+    try:
+        logger.info(log)
+    except NameError:
+        _ = ""
+    print(log)
+
+
 def save_json_file(dictionary: dict, file_name: str, directory_path: str) -> None:
     """Converts a dictionary into a JSON file and saves it for future use.
 
