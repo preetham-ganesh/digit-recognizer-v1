e8ad440fed1188c85fba9595a5598f7ed278c873
Preetham Ganesh <preetham.ganesh2015@gmail.com>
2022-11-07 20:13:18 -0600

    added create_log function

--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,7 @@
 
 
 import os
+import logging
 
 
 def check_directory_path_existence(directory_path: str) -> str:
@@ -22,3 +23,31 @@ def check_directory_path_existence(directory_path: str) -> str:
     if not os.path.isdir(absolute_directory_path):
         os.makedirs(absolute_directory_path)
     return absolute_directory_path
+
+
+def create_log(logger_directory_path: str, log_file_name: str) -> None:
+    """Creates an object for logging terminal output.
+
+    Args:
+        logger_directory_path: A string which contains the location where the log file should be stored.
+        log_file_name: A string which contains the name for the log file.
+
+    Returns:
+        None.
+    """
+    # Type checks arguments.
+    assert isinstance(logger_directory_path, str), "Logger_directory_path is not of type string."
+    assert isinstance(log_file_name, str), "Log_file_name is not of type string."
+
+    # Checks if the following path exists.
+    logger_directory_path = check_directory_path_existence(logger_directory_path)
+
+    # Create and configure logger
+    logging.basicConfig(
+        filename="{}/{}.log".format(logger_directory_path, log_file_name),
+        format="%(asctime)s %(message)s",
+        filemode="w",
+    )
+    global logger
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
