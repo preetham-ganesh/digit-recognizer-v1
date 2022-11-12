2ae75259348224a2b35311f3b9fc76b03e97285a
Preetham Ganesh <preetham.ganesh2021@gmail.com>
2022-11-11 22:43:25 -0600

    added save_json_file function

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,8 @@
 import os
 import logging
 
+import json
+
 
 def check_directory_path_existence(directory_path: str) -> str:
     """Creates the directory path.
@@ -86,3 +88,34 @@ def log_information(log: str) -> None:
     except NameError:
         _ = ""
     print(log)
+
+
+def save_json_file(dictionary: dict, file_name: str, directory_path: str) -> None:
+    """Saves dictionary as a JSON file.
+
+    Converts a dictionary into a JSON file and saves it for future use.
+
+    Args:
+        dictionary: A dictionary which needs to be saved.
+        file_name: A string for the name with which the file has to be saved.
+        directory_path: A string for the path where the file needs to be saved.
+
+    Returns:
+        None.
+    """
+    # Types checks arguments.
+    assert isinstance(dictionary, dict), "Variable dictionary should be of type 'dict'."
+    assert isinstance(file_name, str), "Variable file_name should be of type 'str'."
+    assert isinstance(
+        directory_path, str
+    ), "Variable directory_path should be of type 'str'."
+
+    # Checks if the following path exists.
+    directory_path = check_directory_path_existence(directory_path)
+
+    # Saves the dictionary or list as a JSON file at the file path location.
+    file_path = "{}/{}.json".format(directory_path, file_name)
+    with open(file_path, "w") as out_file:
+        json.dump(dictionary, out_file, indent=4)
+    out_file.close()
+    log_information("{} file saved successfully at {}.".format(file_name, file_path))
