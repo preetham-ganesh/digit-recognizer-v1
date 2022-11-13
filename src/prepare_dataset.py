61260131ef2e4ba5a4933331d1cc67b952a5ff27
Preetham Ganesh <preetham.ganesh2021@gmail.com>
2022-11-12 18:28:03 -0600

    added preprocess_current_dataset function

--- src/prepare_dataset.py
+++ src/prepare_dataset.py
@@ -6,6 +6,8 @@ from typing import Tuple
 
 import pandas as pd
 from sklearn.utils import shuffle
+import tensorflow as tf
+import numpy as np
 
 
 class Dataset:
@@ -48,3 +50,33 @@ class Dataset:
             test_data_end_index:validation_data_end_index
         ]
         self.new_train_data = self.original_train_data[validation_data_end_index:]
+
+    def preprocess_current_dataset(self, new_data: pd.DataFrame) -> Tuple[tf.Tensor]:
+        """Preprocesses current dataset, to produce input (& target) data."""
+        # Asserts type of arguments.
+        assert isinstance(
+            new_data, pd.DataFrame
+        ), "Variable new_data should be of type 'pd.DataFrame'."
+
+        # If label column exists in the data, then both input & target data is extracted.
+        if "label" in list(new_data.columns):
+
+            # Splits the data into the input and target data
+            new_input_data = new_data.drop(columns=["label"])
+            new_target_data = list(new_data["label"])
+
+        # Else input data is extracted.
+        else:
+            new_input_data = new_data
+
+        # Reshapes the input data, converts into float32 format, normalizes the pixel values and converts to tensor.
+        new_input_data = np.array(new_input_data).reshape(
+            (len(new_input_data), 28, 28, 1)
+        )
+        new_input_data = tf.convert_to_tensor(new_input_data, dtype="uint8")
+
+        # If label column exists in the data, then converts target data into categorical format.
+        if "label" in list(new_data.columns):
+            new_target_data = tf.keras.utils.to_categorical(new_target_data)
+            return new_input_data, new_target_data
+        return new_input_data
