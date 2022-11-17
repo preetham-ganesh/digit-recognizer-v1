ca0f4437b1bc23ab9426511c7d149d367350f739
Preetham Ganesh <preetham.ganesh2021@gmail.com>
2022-11-17 13:29:42 -0600

    added set_physical_devices_memory_limit function

--- src/utils.py
+++ src/utils.py
@@ -3,8 +3,16 @@
 
 import os
 import logging
+import warnings
+
+
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+logging.getLogger("tensorflow").setLevel(logging.FATAL)
+warnings.filterwarnings("ignore")
+
 
 import json
+import tensorflow as tf
 
 
 def check_directory_path_existence(directory_path: str) -> str:
@@ -31,14 +39,14 @@ def check_directory_path_existence(directory_path: str) -> str:
     return absolute_directory_path
 
 
-def create_log(logger_directory_path: str, log_file_name: str) -> None:
+def create_log(log_file_name: str, logger_directory_path: str) -> None:
     """Creaters a logger.
 
     Creates an object for logging terminal output.
 
     Args:
-        logger_directory_path: A string for the location where the log file should be stored.
         log_file_name: A string for the name for the log file.
+        logger_directory_path: A string for the location where the log file should be stored.
 
     Returns:
         None.
@@ -148,3 +156,33 @@ def load_json_file(file_name: str, directory_path: str) -> dict:
     # Type checks output.
     assert isinstance(dictionary, dict), "Variable dictionary should be of type 'dict'."
     return dictionary
+
+
+def set_physical_devices_memory_limit() -> None:
+    """Sets memory limit of GPU if found in the system.
+
+    Sets memory limit of GPU if found in the system.
+
+    Args:
+        None.
+
+    Returns:
+        None.
+    """
+    # Lists physical devices in the system.
+    gpu_devices = tf.config.list_physical_devices("GPU")
+
+    # If GPU device is found in the system, then the memory limit is set.
+    if len(gpu_devices) > 0:
+        tf.config.experimental.set_memory_growth(gpu_devices[0], enable=True)
+        gpu_available = True
+    else:
+        gpu_available = False
+
+    if gpu_available:
+        log_information("GPU is available and will be used as accelerator.")
+    else:
+        log_information(
+            "GPU is not available, hence the model will be executed on CPU."
+        )
+    log_information("")
