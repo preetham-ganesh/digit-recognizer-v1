7a3a5b61a9b809cbdf62ea8a9708c3cc25b2bc63
Preetham Ganesh <preetham.ganesh2021@gmail.com>
2022-11-18 17:54:10 -0600

    added generate_model_summary_and_plot function

--- src/model_train_predict.py
+++ src/model_train_predict.py
@@ -6,6 +6,7 @@ import os
 import tensorflow as tf
 
 from src.model import DigitRecognizer
+from src.utils import log_information
 
 
 class LoadTrainValidateModel:
@@ -33,4 +34,29 @@ class LoadTrainValidateModel:
         checkpoint.restore(tf.train.latest_checkpoint(checkpoint_directory_path))
         self.manager = tf.train.CheckpointManager(
             checkpoint, directory=checkpoint_directory_path, max_to_keep=3
+        )
+        log_information("Finished loading model for current configuration.")
+        log_information("")
+
+    def generate_model_summary_and_plot(self) -> None:
+        """Generates summary and plot for loaded model."""
+        # Compiles the model to print model summary.
+        input_dim = (
+            self.model_configuration["final_image_size"],
+            self.model_configuration["final_image_size"],
+            self.model_configuration["n_channels"],
+        )
+        _ = self.model.build((self.model_configuration["batch_size"], *input_dim))
+        log_information(self.model.summary())
+        log_information("")
+
+        # Plots the model and saves it as a PNG file.
+        tf.keras.utils.plot_model(
+            self.model.build_graph(),
+            "{}/results/v{}/utils/model_plot.png".format(
+                self.home_directory_path, self.model_configuration["model_version"]
+            ),
+            show_shapes=True,
+            show_layer_names=True,
+            expand_nested=False,
         )
